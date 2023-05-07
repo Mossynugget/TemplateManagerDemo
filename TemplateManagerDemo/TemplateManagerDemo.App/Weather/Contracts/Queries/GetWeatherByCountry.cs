178358c31ce9d07caad6c5febcd6d18f679e62c3
Mossynugget <moss.rdylan@gmail.com>
2023-05-07 22:15:39 +0200

    Updated code structure

---
+++ TemplateManagerDemo/TemplateManagerDemo.App/Weather/Contracts/Queries/GetWeatherByCountry.cs
@@ -0,0 +1,12 @@
+﻿namespace TemplateManagerDemo.App.Weather.Contracts.Queries
+{
+  using System;
+  using MediatR;
+
+  /// <summary>
+  /// Request to Get Weather By Country
+  /// </summary>
+  public partial class GetWeatherByCountry : IRequest<CountryWeather>
+  {
+  }
+}
