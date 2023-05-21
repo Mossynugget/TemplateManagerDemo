d66cd46ad66dc478041423b4c81b107d9968a63f
Mossynugget <moss.rdylan@gmail.com>
2023-05-21 11:57:27 +0200

    Updated template to include new option

--- TemplateManagerDemo/CodeTemplates/Dotnet/Api/Controller/ApiSnippet.cs
+++ TemplateManagerDemo/CodeTemplates/Dotnet/Api/Controller/ApiSnippet.cs
@@ -1,5 +1,5 @@
 
-    [HttpGet(Name = "GetWeatherForecast")]
+    [HttpGet(Name = "$ContractName:lowercaseDashes$")]
     public async Task<$if:ReturnList$List<$endif:ReturnList$$ReturnedObject$$if:ReturnList$>$endif:ReturnList$> Get([FromBody] $ContractName$ request)
     {
       return await _mediator.Send(request).ConfigureAwait(false);
--- TemplateManagerDemo/CodeTemplates/Dotnet/App/Tests/Mediator/handlerTemplateTest.cs
+++ TemplateManagerDemo/CodeTemplates/Dotnet/App/Tests/Mediator/handlerTemplateTest.cs
@@ -1,7 +1,6 @@
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
-using $setting:solution$.App.$Domain$.Contracts.$RequestType$
 
 namespace $setting:solution$.App.Tests
 {
